7963f7259268cb46b575ce43f0f43fd83ad4e798
Your Name <you@example.com>
2020-06-12 22:44:10 +0200

    ajustement de la route commentaire

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const Router = express.Router()
 const UserController = require ('../controllers/UserController')
-const CommentaireController=require('../controllers/CommentaireController');
+
 
 Router.post("/createuser", UserController.createUser())
 Router.post("/createrole", UserController.createRole())
 Router.get("/allusers", UserController.getUser());
-Router.post("/plaintes/:id_plainte/comments",CommentaireController.createCommentaire());
-Router.get("/plaintes/:id_plainte/comments",CommentaireController.getCommentaire());
-Router.get("/plaintes",CommentaireController.getPlaintes());
 
 module.exports = Router
