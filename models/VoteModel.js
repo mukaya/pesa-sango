c1707abd1ecd32a8e8bbab20305bb79cfe7c6ac7
OseeMukaya <oseemukaya@gmail.com>
2020-06-12 19:01:56 +0200

    modification des models

--- models/VoteModel.js
+++ models/VoteModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const voteSchema = new mongoose.Schema({
     nombreVote:{
-        type: int
+        type: Number
     },
     id_user:{
         type: mongoose.Schema.Types.ObjectId,
--- models/VueModel.js
+++ models/VueModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const vueSchema = new mongoose.Schema({
     nombreVue:{
-        type: int
+        type: Number
     },
     id_user:{
         type: mongoose.Schema.Types.ObjectId,
