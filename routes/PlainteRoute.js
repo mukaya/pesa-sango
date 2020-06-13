76023e7a707045c125b9f8a81a97670a936bbec6
OseeMukaya <oseemukaya@gmail.com>
2020-06-13 14:02:18 +0200

    esperons encore

--- routes/PlainteRoute.js
+++ routes/PlainteRoute.js
@@ -7,7 +7,7 @@ Router.get("/plaintes", PlainteController.getPlainte())
 Router.get("/plainteDesc", PlainteController.getPlainteDesc())
 Router.get("/plainteAsc", PlainteController.getPlainteAsc())
 Router.get("/plainteCategorie", PlainteController.getPlainteCategorie())
-Router.get("/plainte/:id", PlainteController.getPlainteById())
+Router.get("/plaintes/:id", PlainteController.getPlainteById())
 Router.get("/plainte/:categorie", PlainteController.getPlainteCategorie())
 Router.get("/allplaintes", PlainteController.getPages())
 
